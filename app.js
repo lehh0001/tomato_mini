c01e3d69037346b719031090537bfa50ed718334
lehh0001 <1010614165@qq.com>
2020-02-11 15:03:33 +0800

    删除app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com',  //照抄
-    app_id: "	wx3e9f2e50e807dbf9",
-    app_secret: "bd06f681ed3350f5ed9337bd0401a48d",
-    t_app_id: "9f8hSD9GaV4LF57gvRqogxEY",
-    t_app_secret: "ssEwX2VuVEBhV6DqUPvFRQJ1"
-  }
-})
